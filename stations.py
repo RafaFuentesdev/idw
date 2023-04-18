8400d81f639416dc7ec154b7a9f14f139d6d1b64
Rafael Fuentes <rafafuentesdev@gmail.com>
2023-04-18 22:10:25 +0200

    added stations.py

---
+++ stations.py
@@ -0,0 +1,507 @@
+STATION_DICT = {
+  "1. SIERRA MIJAS (MA)": {
+    "id": 60000564,
+    "rainfall": "001P01 PLUVIOMETRO",
+    "temperature": "001M02 TEMPERATURA EXTERIOR"
+  },
+  "3. EMBALSE DE CHARCO REDONDO (CA)": {
+    "id": 82127524,
+    "rainfall": "003P01 PLUVIOMETRO",
+    "temperature": "003M02 TEMPERATURA EXTERIOR"
+  },
+  "4. DEP\u00d3SITO REGULADOR CHARCO RDO (CA)": {
+    "id": 82864346,
+    "rainfall": "004P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "5. TORRE TOMA DE CHARCO REDONDO (CA)": {
+    "id": 82391695,
+    "rainfall": "005P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "6. LOS REALES (MA)": {
+    "id": 67018363,
+    "rainfall": "006P01 PLUVIOMETRO",
+    "temperature": "006M02 TEMPERATURA EXTERIOR"
+  },
+  "7. DEP\u00d3SITO DI-1 (CA)": {
+    "id": 82767415,
+    "rainfall": "007P01 PLUVIOMETRO",
+    "temperature": "007M02 TEMPERATURA EXTERIOR"
+  },
+  "8. EMBALSE DE GUADARRANQUE (CA)": {
+    "id": 77735103,
+    "rainfall": "008P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "9. R\u00cdO HOZGARGANTA (JIMENA) (CA)": {
+    "id": 70214713,
+    "rainfall": "009P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "10. LOMAS DE C\u00c1MARA (CA)": {
+    "id": 70756612,
+    "rainfall": "010P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "11. R\u00cdO GUADIARO(S PABLO BUCEITE) (CA)": {
+    "id": 67670994,
+    "rainfall": "011P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "12. MAJADA DE LAS LOMAS (MA)": {
+    "id": 63180747,
+    "rainfall": "012P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "13. R\u00cdO GENAL (JUBRIQUE) (MA)": {
+    "id": 61874863,
+    "rainfall": "013P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "14. PUJERRA (MA)": {
+    "id": 61332978,
+    "rainfall": "014P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "16. EMBALSE DE LA CONCEPCI\u00d3N (MA)": {
+    "id": 64211254,
+    "rainfall": "016P01 PLUVIOMETRO",
+    "temperature": "016M02 TEMPERATURA EXTERIOR"
+  },
+  "17. OJ\u00c9N (MA)": {
+    "id": 62237810,
+    "rainfall": "017P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "18. EL TORCAL (MA)": {
+    "id": 38996101,
+    "rainfall": "018P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "19. EMBALSE DE CASASOLA (MA)": {
+    "id": 47879263,
+    "rainfall": "019P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "20. EMBALSE DEL LIMONERO (MA)": {
+    "id": 50743101,
+    "rainfall": "020P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "21. DEPURADORA DEL ATABAL (MA)": {
+    "id": 52550097,
+    "rainfall": "021P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "22. M\u00c1LAGA - PASEO DE LA FAROLA (MA)": {
+    "id": 53481654,
+    "rainfall": "022P01 PLUVIOMETRO",
+    "temperature": "022M02 TEMPERATURA EXTERIOR"
+  },
+  "23. PARTIDOR DE GUADARRANQUE (CA)": {
+    "id": 77999246,
+    "rainfall": "023P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "24. DEPURADORA DE MARBELLA (MA)": {
+    "id": 65337284,
+    "rainfall": "024P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "25. SANT\u00d3N PITAR (MA)": {
+    "id": 48199709,
+    "rainfall": "025P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "26. CA\u00d1ETE LA REAL (MA)": {
+    "id": 38521945,
+    "rainfall": "026P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "27. RONDA (MA)": {
+    "id": 50712665,
+    "rainfall": "027P01 PLUVIOMETRO",
+    "temperature": "027M02 TEMPERATURA EXTERIOR"
+  },
+  "28. LAGUNA DE FUENTE PIEDRA (MA)": {
+    "id": 27443990,
+    "rainfall": "028P01 PLUVIOMETRO",
+    "temperature": "028M02 TEMPERATURA EXTERIOR"
+  },
+  "29. EMBALSE DEL GUADALTEBA (MA)": {
+    "id": 39259570,
+    "rainfall": "029P01 PLUVIOMETRO",
+    "temperature": "029M02 TEMPERATURA EXTERIOR"
+  },
+  "31. EMBALSE CONDE DE GUADALHORCE (MA)": {
+    "id": 39704386,
+    "rainfall": "031P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "32. REPETIDOR LA ENCANTADA (MA)": {
+    "id": 41400390,
+    "rainfall": "032P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "33. CANAL DE LA ENCANTADA (MA)": {
+    "id": 41734084,
+    "rainfall": "033P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "34. AZUD DE PAREDONES (MA)": {
+    "id": 44500532,
+    "rainfall": "034P01 PLUVIOMETRO",
+    "temperature": "034M02 TEMPERATURA EXTERIOR"
+  },
+  "35. CO\u00cdN (MA)": {
+    "id": 56858373,
+    "rainfall": "035P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "36. ALCAUC\u00cdN (MA)": {
+    "id": 41986611,
+    "rainfall": "036P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "37. EMBALSE DE LA VI\u00d1UELA (MA)": {
+    "id": 44738820,
+    "rainfall": "037P01 PLUVIOMETRO",
+    "temperature": "037M02 TEMPERATURA EXTERIOR"
+  },
+  "38. R\u00cdO GUADALHORCE (C\u00c1RTAMA) (MA)": {
+    "id": 52549569,
+    "rainfall": "038P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "39. VILLANUEVA DE LA CONCEPCI\u00d3N (MA)": {
+    "id": 39997267,
+    "rainfall": "039P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "40. CASARABONELA (MA)": {
+    "id": 48461867,
+    "rainfall": "040P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "41. COLMENAR (MA)": {
+    "id": 39733631,
+    "rainfall": "041P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "42. ALFARNATEJO (MA)": {
+    "id": 38830605,
+    "rainfall": "042P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "43. R\u00cdO BENAMARGOSA (S. NEGRO) (MA)": {
+    "id": 45558836,
+    "rainfall": "043P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "44. TORROX (MA)": {
+    "id": 51565039,
+    "rainfall": "044P01 PLUVIOMETRO",
+    "temperature": "044M02 TEMPERATURA EXTERIOR"
+  },
+  "45. TORRE DEL MAR (MA)": {
+    "id": 51453195,
+    "rainfall": "045P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "46. R\u00cdO GUADALHORCE (ALJAIMA) (MA)": {
+    "id": 52549386,
+    "rainfall": "046P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "47. L\u00daJAR (GR)": {
+    "id": 47410534,
+    "rainfall": "047P01 PLUVIOMETRO",
+    "temperature": "047M02 TEMPERATURA EXTERIOR"
+  },
+  "48. LA CABRA MONT\u00c9S (GR)": {
+    "id": 46631004,
+    "rainfall": "048P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "49. R\u00cdO VERDE (CAZULAS) (GR)": {
+    "id": 47937741,
+    "rainfall": "049P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "50. V\u00c9LEZ DE BENAUDALLA (GR)": {
+    "id": 45589075,
+    "rainfall": "050P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "51. EMBALSE DE RULES (GR)": {
+    "id": 45130436,
+    "rainfall": "051P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "52. AZUD DE V\u00c9LEZ (GR)": {
+    "id": 47076478,
+    "rainfall": "052P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "53. PARTIDOR DE CA\u00d1IZARES (GR)": {
+    "id": 49773203,
+    "rainfall": "053P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "54. AZUD DEL V\u00cdNCULO (GR)": {
+    "id": 49800991,
+    "rainfall": "054P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "55. PADUL (GR)": {
+    "id": 34092416,
+    "rainfall": "055P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "57. BAYARCAL (AL)": {
+    "id": 34887182,
+    "rainfall": "057P01 PLUVIOMETRO",
+    "temperature": "057M02 TEMPERATURA EXTERIOR"
+  },
+  "58. EMBALSE DE BENINAR (AL)": {
+    "id": 44117319,
+    "rainfall": "058P01 PLUVIOMETRO",
+    "temperature": "058M02 TEMPERATURA EXTERIOR"
+  },
+  "59. MURTAS (GR)": {
+    "id": 44005797,
+    "rainfall": "059P01 PLUVIOMETRO",
+    "temperature": "059M02 TEMPERATURA EXTERIOR"
+  },
+  "60. MOTRIL (GR)": {
+    "id": 51816726,
+    "rainfall": "060P01 PLUVIOMETRO.",
+    "temperature": "060M02 TEMPERATURA EXTERIOR"
+  },
+  "61. LOS GUAJARES (GR)": {
+    "id": 45352378,
+    "rainfall": "061P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "63. ALBU\u00d1UELAS (GR)": {
+    "id": 40820611,
+    "rainfall": "063P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "64. EMBALSE DE B\u00c9ZNAR (GR)": {
+    "id": 41627238,
+    "rainfall": "064P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "65. LANJAR\u00d3N (GR)": {
+    "id": 38221742,
+    "rainfall": "065P01 PLUVIOMETRO",
+    "temperature": "065M02 TEMPERATURA EXTERIOR"
+  },
+  "66. CAPILEIRA (GR)": {
+    "id": 37346377,
+    "rainfall": "066P01 PLUVIOMETRO",
+    "temperature": "066M02 TEMPERATURA EXTERIOR"
+  },
+  "67. TREVELEZ (GR)": {
+    "id": 37430090,
+    "rainfall": "067P01 PLUVIOMETRO",
+    "temperature": "067M02 TEMPERATURA EXTERIOR"
+  },
+  "68. PUERTO DE LA RAGUA (AL)": {
+    "id": 31537017,
+    "rainfall": "068P01 PLUVIOMETRO",
+    "temperature": "068M02 TEMPERATURA EXTERIOR"
+  },
+  "69. R\u00cdO GUADALFEO (ALMEG\u00cdJAR) (GR)": {
+    "id": 43032081,
+    "rainfall": "069P01 PLUVI\u00d3METRO",
+    "temperature": False
+  },
+  "70. CONTRAVIESA (GR)": {
+    "id": 45312168,
+    "rainfall": "070P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "71. CASTALA (AL)": {
+    "id": 43700630,
+    "rainfall": "071P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "72. ALBU\u00d1OL (GR)": {
+    "id": 50261067,
+    "rainfall": "072P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "75. SIERRA ALHAMILLA (AL)": {
+    "id": 37155220,
+    "rainfall": "075P01 PLUVIOMETRO",
+    "temperature": "075M02 TEMPERATURA EXTERIOR"
+  },
+  "76. SIERRA DE G\u00c1DOR (AL)": {
+    "id": 43881818,
+    "rainfall": "076P01 PLUVIOMETRO",
+    "temperature": "076M02 TEMPERATURA EXTERIOR"
+  },
+  "77. F\u00c9LIX (AL)": {
+    "id": 44466179,
+    "rainfall": "077P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "78. PUNTA SABINAR (AL)": {
+    "id": 55781431,
+    "rainfall": "078P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "79. CANAL BENINAR-AGUADULCE (AL)": {
+    "id": 50220283,
+    "rainfall": "079P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "80. SIERRA DE LOS FILABRES (AL)": {
+    "id": 20849236,
+    "rainfall": "080P01 PLUVIOMETRO",
+    "temperature": "080M02 TEMPERATURA EXTERIOR"
+  },
+  "81. ORIA (AL)": {
+    "id": 6461856,
+    "rainfall": "081P01 PLUVIOMETRO",
+    "temperature": "081M02 TEMPERATURA EXTERIOR"
+  },
+  "82. TAHAL (AL)": {
+    "id": 22170287,
+    "rainfall": "082P01 PLUVIOMETRO",
+    "temperature": "082M02 TEMPERATURA EXTERIOR"
+  },
+  "83. SIERRA ALMAGRO (AL)": {
+    "id": 13580918,
+    "rainfall": "083P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "84. EMBALSE CUEVAS DEL ALMANZORA (AL)": {
+    "id": 16013498,
+    "rainfall": "084P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "85. R\u00cdO ALMANZORA (CANTORIA) (AL)": {
+    "id": 14914363,
+    "rainfall": "085P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "86. R\u00cdO ALMANZORA (SER\u00d3N) (AL)": {
+    "id": 14760219,
+    "rainfall": "086P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "87. ALBOX (AL)": {
+    "id": 7894152,
+    "rainfall": "087P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "88. EL SALTADOR (AL)": {
+    "id": 10689493,
+    "rainfall": "088P01 PLUVIOMETRO",
+    "temperature": "088M02 TEMPERATURA EXTERIOR"
+  },
+  "89. ALMER\u00cdA (AL)": {
+    "id": 46593728,
+    "rainfall": "089P01 PLUVIOMETRO",
+    "temperature": "089M02 TEMPERATURA EXTERIOR"
+  },
+  "90. ANDARAX Y NACIMIENTO (TERQUE) (AL)": {
+    "id": 37515912,
+    "rainfall": "090P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "91. OHANES (AL)": {
+    "id": 32914108,
+    "rainfall": "091P01 PLUVIOMETRO",
+    "temperature": "091M02 TEMPERATURA EXTERIOR"
+  },
+  "92. FI\u00d1ANA (AL)": {
+    "id": 25879876,
+    "rainfall": "092P01 PLUVIOMETRO",
+    "temperature": "092M02 TEMPERATURA EXTERIOR"
+  },
+  "93. RAMBLA DE TABERNAS (AL)": {
+    "id": 35292277,
+    "rainfall": "093P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "94. G\u00c9RGAL (AL)": {
+    "id": 29175478,
+    "rainfall": "094P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "95. VELEFIQUE (AL)": {
+    "id": 24338416,
+    "rainfall": "095P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "96. SORBAS (AL)": {
+    "id": 30191705,
+    "rainfall": "096P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "97. N\u00cdJAR (AL)": {
+    "id": 43856325,
+    "rainfall": "097P01 PLUVIOMETRO",
+    "temperature": "097M02 TEMPERATURA EXTERIOR"
+  },
+  "101. LA ARA\u00d1A (MA)": {
+    "id": 53746095,
+    "rainfall": "101P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "102. SAN ENRIQUE DE GUADIARO (CA)": {
+    "id": 77735734,
+    "rainfall": "102P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "103. R\u00cdO GUADIARO (TR. MAJACEITE) (MA)": {
+    "id": 61082179,
+    "rainfall": "103P01 PLUVIOMETRO",
+    "temperature": "103M02 TEMPERATURA EXTERIOR"
+  },
+  "104. R\u00cdO GRANDE (LAS MILLANAS) (MA)": {
+    "id": 54036032,
+    "rainfall": "104P01 PLUVI\u00d3METRO",
+    "temperature": "104M02 TEMPERATURA EXTERIOR"
+  },
+  "105. LAS GOLONDRINAS (MA)": {
+    "id": 56857745,
+    "rainfall": "105P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "125. DEPURADORA DEL TRAPICHE (MA)": {
+    "id": 47894489,
+    "rainfall": "125P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "126. FAHALA (MA)": {
+    "id": 51979527,
+    "rainfall": "126P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "127. R\u00cdO GUADALHORCE (BOBADILLA) (MA)": {
+    "id": 33268614,
+    "rainfall": "127P01 PLUVI\u00d3METRO",
+    "temperature": False
+  },
+  "128. R\u00cdO TUR\u00d3N (ARDALES) (MA)": {
+    "id": 42609528,
+    "rainfall": "128P01 PLUVI\u00d3METRO",
+    "temperature": False
+  },
+  "129. R\u00cdO GUADALTEBA (AFORO TEBA) (MA)": {
+    "id": 39759544,
+    "rainfall": "129P01 PLUVIOMETRO",
+    "temperature": False
+  },
+  "130. R\u00cdO GUADALHORCE (ARCHIDONA) (MA)": {
+    "id": 30906302,
+    "rainfall": "130P01 PLUVI\u00d3METRO",
+    "temperature": "130M02 TEMPERATURA EXTERIOR"
+  }
+}
